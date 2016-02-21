286c21af3764ed526093a03de042b4ff054162bd
Joel <joelcastillo@me.com>
2016-02-21 23:04:41 +0000

    updated:

--- BandBuds/bba/models.py
+++ BandBuds/bba/models.py
@@ -1,29 +1,30 @@
 from django.db import models
 
 class User(models.Model):
-	user_id     = models.CharField(max_length=128, unique=True)
+	user_id     = models.CharField(primary_key=True)
 	f_Name	    = models.CharField(max_length=128, unique=False)
 	s_Name      = models.CharField(max_length=128, unique=False)
 	dob	        = models.DateField(default=models.DateField.auto_now_add)
 	smokes	    = models.BooleanField()
 	gender      = models.CharField(max_length=128, unique=False)
 	alcohol     = models.BooleanField()
+    email       = models.EmailField(max_length=254)
 
     def __unicode__(self):
         return self.user_id
 
 
 class Buddy(models.model):
-    user_Id     = models.CharField(primary_key=True,max_length=50)
-    user_Id     = models.ForeignKey("user_Id")
+    user_Id     = models.CharField(primary_key=True)
+    user_Id     = models.ForeignKey(user_Id)
 
- def __unicode__(self):
-        return 
+    def __unicode__(self):
+        return self.user_id
 
 class Band(models.Model):
-    name 	    = models.CharField(primary_key=True, max_length=50)
-    city 	    = models.CharField(primary_key=True, max_length=50)
-    country     = models.CharField(primary_key=True, max_length=50)
+    name 	    = models.CharField(primary_key=True)
+    city 	    = models.CharField(primary_key=True)
+    country     = models.CharField(primary_key=True)
     formed 	    = models.IntegerField(default=0)
     genre 	    = models.CharField(max_length=25)
 
@@ -31,40 +32,40 @@ class Band(models.Model):
         return self.name + ' ' + self.city + ' ' + self.country
 
 class Liked_Band(models.model):
-    name        = models.ForeignKey(Name, max_length=50)
-    city        = models.ForeignKey(City, max_length=50)
-    country     = models.ForeignKey(Country, max_length=50)
+    name        = models.ForeignKey(Name)
+    city        = models.ForeignKey(City)
+    country     = models.ForeignKey(Country)
 
     def __unicode__(self):
-        return 
+        return self.name
 
 
 class Performing_Band(models.model):
-    name        = models.ForeignKey(Name, max_length=50)
-    city        = models.ForeignKey(City, max_length=50)
-    country     = models.ForeignKey(Country, max_length=50)
-    gig_Id      = models.ForeignKey(Gig_Id, max_length=50, unique=True)
+    name        = models.ForeignKey(Name)
+    city        = models.ForeignKey(City)
+    country     = models.ForeignKey(Country)
+    gig_Id      = models.ForeignKey(Gig_Id)
 
- def __unicode__(self):
-        return 
+    def __unicode__(self):
+        return self.gig_Id
 
 class Gig(models.model):
     date        = models.DateField(default=models.DateField.auto_now_add)
     time        = models.DateField(default=models.DateField.auto_now_add)
-    country     = models.ForeignKey(Country, max_length=128, unique=True)
-    city        = models.ForeignKey(City, max_length=128, unique=True)
-    venue_Id    = models.ForeignKey(Venue_Id, max_length=128, unique=True)
-    band_Name   = models.ForeignKey(Band_Name, max_length=128)
+    country     = models.ForeignKey(Country)
+    city        = models.ForeignKey(City)
+    venue_Id    = models.ForeignKey(Venue_Id)
+    band_Name   = models.ForeignKey(Band_Name)
 
- def __unicode__(self):
-        return 
+    def __unicode__(self):
+        return self.band_Name
 
 class Gig_Attendance(models.model):
-    gig_Id      = models.CharField(primary_key=True, max_length=50)
-    user_Id     = models.ForeignKey(User_Id, max_length=128, unique=True)
+    gig_Id      = models.CharField(primary_key=True)
+    user_Id     = models.ForeignKey(User_Id)
 
- def __unicode__(self):
-        return 
+    def __unicode__(self):
+        return self.gig_Id
 
 class Venue(models.model):
     city        = models.CharField(max_length=50)
@@ -73,5 +74,5 @@ class Venue(models.model):
     building_No = models.IntegerField(default=0)
     street      = models.CharField(max_length=128)
 
- def __unicode__(self):
-        return self.name
+    def __unicode__(self):
+        return self.street
