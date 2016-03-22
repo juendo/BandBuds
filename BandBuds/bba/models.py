93fcb367414027999a940f0b6985d622f80d18de
Stevo <stevestando@yahoo.co.uk>
2016-03-22 22:03:52 +0000

    added comments to models and populate script

--- BandBuds/bba/models.py
+++ BandBuds/bba/models.py
@@ -7,19 +7,21 @@ from django.core.validators import validate_email
 from django.template.defaultfilters import slugify
 from datetime import date
 
+# choices for the gender data field
 GENDER_CHOICES = (
     ('Undisclosed', 'Undisclosed'),
     ('Female', 'Female'),
     ('Male', 'Male'),
 )
 
-
 class UserProfile(models.Model):
 
+    # Django defined User class
     user = models.OneToOneField(User)
+
+    # additional fields for UserProfile (all given default values)
     dob = models.DateField(default=date.today())
     gender = models.CharField(default="Undisclosed", max_length=200, choices=GENDER_CHOICES)
-
     smokes = models.IntegerField(default=0)
     drinks = models.IntegerField(default=0)
     dances = models.IntegerField(default=0)
@@ -36,6 +38,7 @@ class UserProfile(models.Model):
         return self.user.username
 
 class Band(models.Model):
+    # stores name of band and URL to image of band from Songkick website
     name = models.CharField(max_length=128, unique=False)
     image_Ref = models.CharField(max_length=128, unique=False)
     slug = models.SlugField()
@@ -48,6 +51,7 @@ class Band(models.Model):
         return self.name
 
 class LikedBand(models.Model):
+    # links a Band and UserProfile object
     band = models.ForeignKey(Band)
     user = models.ForeignKey(UserProfile)
 
@@ -55,6 +59,7 @@ class LikedBand(models.Model):
         return str(self.band) + ' ' + str(self.user)
 
 class DisLikedBand(models.Model):
+    # links a Band and UserProfile object
     band = models.ForeignKey(Band)
     user = models.ForeignKey(UserProfile)
 
@@ -62,6 +67,7 @@ class DisLikedBand(models.Model):
         return self.band + ' ' + self.user
 
 class Venue(models.Model):
+    # venue information
     venue_id = models.IntegerField(default=0)
     name = models.CharField(max_length=128)
     # latitude and logitude?
@@ -70,6 +76,7 @@ class Venue(models.Model):
         return self.name
 
 class Gig(models.Model):
+    # gig information
     gig_id = models.IntegerField(default=0)
     date = models.DateField()
     time = models.CharField(max_length=128)
@@ -81,6 +88,7 @@ class Gig(models.Model):
         return self.venue.name + ' ' + self.band.name
 
 class PerformingBand(models.Model):
+    # links a Band to a Gig object
     band = models.ForeignKey(Band)
     gig = models.ForeignKey(Gig)
 
@@ -88,6 +96,7 @@ class PerformingBand(models.Model):
         return self.band + self.gig
 
 class GigAttendance(models.Model):
+    # links a Gig to a UserProfile object
     gig = models.ForeignKey(Gig)
     user = models.ForeignKey(UserProfile)
     slug = models.SlugField()
@@ -101,6 +110,10 @@ class GigAttendance(models.Model):
 
 
 class Buddy(models.Model):
+    # links two UserProfile objects
+    # this represents a 'buddy request' state
+    # if accept is set to true, the request is accepted and
+    # the two UserProfiles are deemed to be 'buddies'
     user = models.ForeignKey(UserProfile)
     buddy = models.ForeignKey(UserProfile, related_name='+')
     gig = models.ForeignKey(Gig)
@@ -113,17 +126,3 @@ class Buddy(models.Model):
 
     def __unicode__(self):
         return self.user + ' ' + self.buddy + ' ' + self.gig.band + ' request accepted: ' + self.accept
-
-
-
-
-
-
-
-class Nudge(models.Model):
-    nudger = models.ForeignKey(User)
-    nudgee = models.ForeignKey(UserProfile)
-    gig = models.ForeignKey(Gig)
-
-    def __unicode__(self):
-        return nudger.user.username + ' nudged ' + nudgee.user.username + ' about ' + gig 
